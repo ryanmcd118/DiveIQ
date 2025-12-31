0edbdb2ca39d1f62810d05b0c5fc58b24b2035a5
ryanmcd <rp.mcdan@gmail.com>
2025-12-31 14:26:31 -0500

    added types for custom auth fields

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,38 +1,32 @@
 import "next-auth";
+import { DefaultSession, DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
-      email: string;
-      firstName: string | null;
-      lastName: string | null;
-      image?: string;
+      firstName?: string | null;
+      lastName?: string | null;
       avatarUrl?: string | null;
       sessionVersion?: number;
-    };
+    } & DefaultSession["user"];
   }
 
-  interface User {
-    id: string;
-    email: string;
-    firstName: string | null;
-    lastName: string | null;
-    image?: string;
+  interface User extends DefaultUser {
+    firstName?: string | null;
+    lastName?: string | null;
     avatarUrl?: string | null;
+    sessionVersion?: number;
   }
 }
 
 declare module "next-auth/jwt" {
   interface JWT {
-    id: string;
-    email: string;
-    firstName: string | null;
-    lastName: string | null;
+    id?: string;
+    firstName?: string | null;
+    lastName?: string | null;
     avatarUrl?: string | null;
     sessionVersion?: number;
     invalidated?: boolean;
   }
 }
-
-
